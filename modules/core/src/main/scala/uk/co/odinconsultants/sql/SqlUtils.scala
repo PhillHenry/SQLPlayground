ab4206d1a4d438c53155d63b3797803c85784023
PhillipHenry <PhillHenry@gmail.com>
2024-02-08 16:50:45 +0000

    Working test

--- modules/core/src/main/scala/uk/co/odinconsultants/sql/SqlUtils.scala
+++ modules/core/src/main/scala/uk/co/odinconsultants/sql/SqlUtils.scala
@@ -3,11 +3,11 @@ package uk.co.odinconsultants.sql
 import java.lang.reflect.Field
 
 object SqlUtils {
-  
+
   def toSimpleName(x: Class[?]): String = x.getSimpleName
 
   def ddlFields(clazz: Class[?]): String = {
-    val jvmToSql: Map[Class[?], String] = Map(classOf[String] -> "VARCHAR").withDefault { toSimpleName }
+    val jvmToSql: Map[Class[?], String] = Map(classOf[String] -> "VARCHAR(1024)").withDefault { toSimpleName }
     clazz.getDeclaredFields.toList
       .map { case(field: Field) =>
         s"${field.getName} ${jvmToSql(field.getType)}"
--- modules/core/src/test/scala/uk/co/odinconsultants/sql/CommonSQLSpec.scala
+++ modules/core/src/test/scala/uk/co/odinconsultants/sql/CommonSQLSpec.scala
@@ -14,13 +14,14 @@ import uk.co.odinconsultants.sql.MSSqlMain.{xa, ctx}
 import uk.co.odinconsultants.sql.SqlServerUtils.ddlIfTableDoesNotExist
 import uk.co.odinconsultants.sql.SqlUtils.ddlFields
 import io.getquill.*
+import io.getquill.autoQuote
 
 case class Address(id: Int, location: String)
 
 case class Customer(id: Int, name: String, address: Int)
 
 class CommonSQLSpec extends SpecPretifier with GivenWhenThen {
-  
+
   import ctx.*
 
   implicit def logger[F[_]: Sync]: Logger[F] = Slf4jLogger.getLogger[F]
@@ -28,19 +29,20 @@ class CommonSQLSpec extends SpecPretifier with GivenWhenThen {
   def tableNameFor(x: Class[?]) = x.getSimpleName.replace("$", "_")
 
   def execute(sql: String, xa: Transactor[IO]): IO[Int] =
-    Fragment
+    IO.println(s"Running: $sql") *> Fragment
       .const(sql)
       .update
       .run
       .transact(xa)
 
-   def someAddresses(n: Int): Seq[Address] = (0 to n).map(i => Address(i, s"address $i"))
+  def someAddresses(n: Int): Seq[Address] = (0 until n).map(i => Address(i, s"address $i"))
 
-   def someCustomers(n: Int, addressMod: Int): Seq[Customer] = (0 to n).map(i => Customer(i, s"name $i", i % addressMod))
+  def someCustomers(n: Int, addressMod: Int): Seq[Customer] = (0 until n).map(i => Customer(i, s"name $i", i % addressMod))
 
-   def check[T] = {
-//     ctx.run(query[T]
-   }
+  def dropAfter(sql: String, dropTable: String): Resource[IO, Unit] = Resource
+      .make {
+        execute(sql, xa).void
+      }(_ => execute(s"DROP TABLE $dropTable", xa) *> IO.println(s"Dropped $dropTable"))
 
   "Created table" should {
     "allow inserts" in {
@@ -54,15 +56,34 @@ class CommonSQLSpec extends SpecPretifier with GivenWhenThen {
            |PRIMARY KEY (id),
            |CONSTRAINT fk_$addressTable FOREIGN KEY(address) REFERENCES $addressTable(id))""".stripMargin
       val customerDDL               = ddlIfTableDoesNotExist(customerTable, createCustomerSQL)
-      val program                   = for {
-        _ <- IO(Given(s"SQL:\n${formatSQL(addressDDL)}\n${formatSQL(customerDDL)}"))
-        _ <- IO(When("we execute the SQL"))
-        _ <- execute(addressDDL, xa)
-        _ <- execute(customerDDL, xa)
-        _ <- IO(Then(s"the tables $customerTable and $addressTable exist but are empty"))
-        _ <- IO(assert(ctx.run(query[Customer]).size == 0))
-        _ <- IO(assert(ctx.run(query[Address]).size == 0))
-      } yield ()
+      val program = (
+          for {
+            _ <- Resource.make(IO(Given(s"SQL:\n${formatSQL(addressDDL)}\n${formatSQL(customerDDL)}")))(_ => IO.unit)
+            _ <- Resource.make(IO(When("we execute the SQL")))(_ => IO.unit)
+            _ <- dropAfter(addressDDL, addressTable)
+            _ <- dropAfter(customerDDL, customerTable)
+          } yield ()
+        ).use { case _ =>
+          for {
+            _ <- IO(And("we populate those tables"))
+            n = 5
+            _ <- IO.println(s"Creating $n addresses") *> IO {
+              val q = quote {
+                liftQuery(someAddresses(n).toList).foreach(a => query[Address].insertValue(a))
+              }
+              ctx.run(q)
+            }
+            _ <- IO.println(s"Creating $n customers") *> IO {
+              val q = quote {
+                liftQuery(someCustomers(n, n).toList).foreach(a => query[Customer].insertValue(a))
+              }
+              ctx.run(q)
+            }
+            _ <- IO(Then(s"the tables $customerTable and $addressTable have $n rows in them"))
+            _ <- IO(assert(ctx.run(query[Customer]).size == n))
+            _ <- IO(assert(ctx.run(query[Address]).size == n))
+          } yield ()
+      }
       program.unsafeRunSync()
     }
   }
