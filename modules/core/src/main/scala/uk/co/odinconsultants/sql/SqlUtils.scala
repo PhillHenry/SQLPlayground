8bd66392ccad4f6c35192779a448dd6ce61d4357
PhillipHenry <PhillHenry@gmail.com>
2024-02-08 14:42:17 +0000

    Table creation.

--- modules/core/src/main/scala/uk/co/odinconsultants/sql/SqlUtils.scala
+++ modules/core/src/main/scala/uk/co/odinconsultants/sql/SqlUtils.scala
@@ -3,12 +3,16 @@ package uk.co.odinconsultants.sql
 import java.lang.reflect.Field
 
 object SqlUtils {
+  
+  def toSimpleName(x: Class[?]): String = x.getSimpleName
 
-  def ddlFields(clazz: Class[?]): String =
+  def ddlFields(clazz: Class[?]): String = {
+    val jvmToSql: Map[Class[?], String] = Map(classOf[String] -> "VARCHAR").withDefault { toSimpleName }
     clazz.getDeclaredFields.toList
       .map { case(field: Field) =>
-        s"${field.getName} ${field.getType.getSimpleName}"
+        s"${field.getName} ${jvmToSql(field.getType)}"
       }
       .mkString(",\n ")
+  }
 
 }
--- modules/core/src/test/scala/uk/co/odinconsultants/sql/CommonSQLSpec.scala
+++ modules/core/src/test/scala/uk/co/odinconsultants/sql/CommonSQLSpec.scala
@@ -1,8 +1,16 @@
 package uk.co.odinconsultants.sql
-import cats.effect.IO
-import org.scalatest.GivenWhenThen
-import uk.co.odinconsultants.documentation_utils.SpecPretifier
+
+import cats.*
+import cats.effect.*
 import cats.effect.unsafe.implicits.global
+import doobie.*
+import doobie.implicits.*
+import doobie.util.transactor.Transactor
+import org.scalatest.GivenWhenThen
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+import uk.co.odinconsultants.documentation_utils.SpecPretifier
+import uk.co.odinconsultants.sql.MSSqlMain.xa
 import uk.co.odinconsultants.sql.SqlServerUtils.ddlIfTableDoesNotExist
 import uk.co.odinconsultants.sql.SqlUtils.ddlFields
 
@@ -12,15 +20,38 @@ case class Customer(id: Int, name: String, address: Int)
 
 class CommonSQLSpec extends SpecPretifier with GivenWhenThen {
 
+  implicit def logger[F[_]: Sync]: Logger[F] = Slf4jLogger.getLogger[F]
+
   def tableNameFor(x: Class[?]) = x.getSimpleName.replace("$", "_")
+
+  def execute(sql: String, xa: Transactor[IO]): IO[Int] =
+    Fragment
+      .const(sql)
+      .update
+      .run
+      .transact(xa)
+
+   def someAddresses(n: Int): Seq[Address] = (0 to n).map(i => Address(i, s"address $i"))
+
+   def someCustomers(n: Int, addressMod: Int): Seq[Customer] = (0 to n).map(i => Customer(i, s"name $i", i % addressMod))
 
   "Created table" should {
     "allow inserts" in {
-      val customerTable     = tableNameFor(classOf[Customer])
-      val createSQL: String = s"CREATE TABLE $customerTable (${ddlFields(classOf[Customer])})"
-      val sql               = ddlIfTableDoesNotExist(customerTable, createSQL)
-      val program           = for {
-        _ <- IO(Given(s"SQL:\n${formatSQL(sql)}"))
+      val customerTable             = tableNameFor(classOf[Customer])
+      val addressTable              = tableNameFor(classOf[Address])
+      val addressSQL: String        =
+        s"CREATE TABLE $addressTable (${ddlFields(classOf[Address])}, PRIMARY KEY (id))"
+      val addressDDL                = ddlIfTableDoesNotExist(addressTable, addressSQL)
+      val createCustomerSQL: String =
+        s"""CREATE TABLE $customerTable (${ddlFields(classOf[Customer])},
+           |PRIMARY KEY (id),
+           |CONSTRAINT fk_$addressTable FOREIGN KEY(address) REFERENCES $addressTable(id))""".stripMargin
+      val customerDDL               = ddlIfTableDoesNotExist(customerTable, createCustomerSQL)
+      val program                   = for {
+        _ <- IO(Given(s"SQL:\n${formatSQL(addressDDL)}\n${formatSQL(customerDDL)}"))
+        _ <- IO(When("we execute the SQL"))
+        _ <- execute(addressDDL, xa)
+        _ <- execute(customerDDL, xa)
       } yield ()
       program.unsafeRunSync()
     }
